bdacb1cfc0cb4c21bfbe82d5e0f94755f91f7d76
mrsandy1965 <8055sandesh8055@gmail.com>
2026-03-05 00:01:00 +0530

    chore: clean up vite config formatting

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-import {resolve , dirname} from 'path'
+import { resolve, dirname } from 'path'
 import { fileURLToPath } from 'url'
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindcss()],
+  plugins: [
+    react(),
+    tailwindcss(),
+  ],
   resolve: {
     alias: {
       '#components': resolve(dirname(fileURLToPath(import.meta.url)), 'src/components'),
