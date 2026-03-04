561b8bdf5d41f0874ecd660c8e7371402be5d028
mrsandy1965 <8055sandesh8055@gmail.com>
2026-03-04 23:28:39 +0530

    fix: split eslint config for Node server vs React frontend - fixes false-positive 'process is not defined' errors

--- eslint.config.js
+++ eslint.config.js
@@ -6,8 +6,24 @@ import { defineConfig, globalIgnores } from 'eslint/config'
 
 export default defineConfig([
   globalIgnores(['dist']),
+
+  // ---- Node.js server files ----
   {
-    files: ['**/*.{js,jsx}'],
+    files: ['server/**/*.js'],
+    extends: [js.configs.recommended],
+    languageOptions: {
+      ecmaVersion: 2022,
+      globals: globals.node,
+      sourceType: 'module',
+    },
+    rules: {
+      'no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+    },
+  },
+
+  // ---- React / frontend files ----
+  {
+    files: ['src/**/*.{js,jsx}'],
     extends: [
       js.configs.recommended,
       reactHooks.configs.flat.recommended,
